d9bba92a91eec30ebcf926a6955f969f5e5b5216
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-04-15 15:04:53 -0400

    expose timeout knobs to boot command

--- vcl/scripts/boot.py
+++ vcl/scripts/boot.py
@@ -36,6 +36,10 @@ def add_parser(p):
                    help='The machine file to write')
     p.add_argument('--prefix', '-P', metavar='STR', default='',
                    help='Prefix the name (not hostname) with this string')
+    p.add_argument('--wait-until-active-timeout', '-a', default=60, type=int,
+                   help='Number of seconds to wait for a node to become ACTIVE before giving up')
+    p.add_argument('--wait-until-active-poll', '-A', default=1, type=int,
+                   help='Number of seconds to wait between polling a new instance to see if it is ACTIVE')
 
 
 def main(opts):
@@ -48,7 +52,10 @@ def main(opts):
     provider = opts.provider or spec.defaults.provider
 
     module = __PROVIDERS[provider]
-    machines = module.boot(nodes, prefix=opts.prefix, dry_run=opts.dry_run)
+    machines = module.boot(nodes, prefix=opts.prefix, dry_run=opts.dry_run,
+                           waitForActiveSleep=opts.wait_until_active_poll,
+                           waitForActiveTimeout=opts.wait_until_active_timeout,
+                           )
 
     with open(opts.machines, 'w') as fd: fd.write('')
 
